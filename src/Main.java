919428ad0693966ebdbea1c9f04bd03f999af455
wojciknatalia <nataliaaawojcik@gmail.com>
2019-04-21 13:32:19 +0200

    Created DisplayImage

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        DisplayImage image=new DisplayImage(args[0]);
+    }
+}
